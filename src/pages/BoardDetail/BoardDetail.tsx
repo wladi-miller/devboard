a41dd12df2cbb9ab575cca8433f9753df2eba242
Wladimir Miller <wladim.miller@gmail.com>
2026-07-30 09:53:37 +0200

    create BoardDetail

--- src/pages/BoardDetail/BoardDetail.tsx
+++ src/pages/BoardDetail/BoardDetail.tsx
@@ -1,7 +1,63 @@
+import { Button } from "../../components/ui/button"
+import { ArrowLeft, Pencil, Check, X } from "lucide-react"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { Input } from "@/components/ui/input"
+
 export default function BoardDetail() {
+  const [isEditName, setIsEditName] = useState(false)
+  const [boardName, setBoardName] = useState("Name des Boards")
+
+  function RenderBoardDetail() {
+    if (isEditName) {
+      return (
+        <div className="flex flex-row items-center gap-2">
+          <Input
+            value={boardName}
+            className="w-96"
+            onChange={(e) => setBoardName(e.target.value)}
+          />
+          <Button
+            variant="ghost"
+            size="icon-lg"
+            onClick={() => setIsEditName(false)}
+          >
+            <Check />
+          </Button>
+          <Button
+            variant="ghost"
+            size="icon-lg"
+            onClick={() => setIsEditName(false)}
+          >
+            <X />
+          </Button>
+        </div>
+      )
+    } else {
+      return (
+        <div className="flex flex-row items-center gap-2">
+          <h1 className="text-2xl font-bold">{boardName}</h1>
+          <Button
+            variant="ghost"
+            size="icon-lg"
+            onClick={() => setIsEditName(true)}
+          >
+            <Pencil className="h-5 w-5" />
+          </Button>
+        </div>
+      )
+    }
+  }
   return (
-    <div>
-      <h1>Board Detail</h1>
+    <div className="container">
+      <div className="flex flex-row items-center gap-2">
+        <Link to="/boards">
+          <Button variant="ghost" size="icon-lg">
+            <ArrowLeft className="h-5 w-5" />
+          </Button>
+        </Link>
+        {RenderBoardDetail()}
+      </div>
     </div>
   )
 }
